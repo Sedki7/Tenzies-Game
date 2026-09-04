b2125b11121dcbd4d960b631c7cac12878c1fa07
Sedki7 <sedkishabou7@gmail.com>
2026-09-05 00:52:39 +0100

    add sound effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,81 @@
-import { useState } from "react"
-import Die from "./Die"
-import { nanoid } from "nanoid"
-import Confetti from "react-confetti"
-import { useWindowSize } from 'react-use'
+import { useRef, useState } from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
+import YroliSound from "./assets/Yrouli1.mp3";
+import YroliFullSound from "./assets/Yrouli.mp3";
+export default function App() {
+    const audioRef = useRef(new Audio(YroliSound));
+    const fullAudioRef = useRef(new Audio(YroliFullSound));
 
-
-export default function App() { 
-    const [dice, setDice] = useState(() => generateAllNewDice())
-    const { width, height } = useWindowSize()
-
-    const gameWon = dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-
-
-    function generateAllNewDice() {
-        return new Array(10)
-            .fill(0)
-            .map(() => ({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            }))
+    const [dice, setDice] = useState(() => generateAllNewDice());
+    const { width, height } = useWindowSize();
+    const gameWon =
+        dice.every((die) => die.isHeld) &&
+        dice.every((die) => die.value === dice[0].value);
+    if (gameWon) {
+        const song = audioRef.current;
+        console.log("sdasd");
+        song.pause();
+        const fullSong = fullAudioRef.current;
+        fullSong.currentTime = 0;
+        fullSong.play();
     }
-    
+    function generateAllNewDice() {
+        return new Array(10).fill(0).map(() => ({
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid(),
+        }));
+    }
+
     function rollDice() {
+        const song = audioRef.current;
+        song.currentTime = 0;
+        song.play();
         if (!gameWon) {
-            setDice(oldDice => oldDice.map(die =>
-                die.isHeld ?
-                    die :
-                    { ...die, value: Math.ceil(Math.random() * 6) }
-            ))
+            setDice((oldDice) =>
+                oldDice.map((die) =>
+                    die.isHeld
+                        ? die
+                        : { ...die, value: Math.ceil(Math.random() * 6) },
+                ),
+            );
         } else {
-            setDice(generateAllNewDice())
+            setDice(generateAllNewDice());
         }
     }
 
     function hold(id) {
-        setDice(oldDice => oldDice.map(die =>
-            die.id === id ?
-                { ...die, isHeld: !die.isHeld } :
-                die
-        ))
+        setDice((oldDice) =>
+            oldDice.map((die) =>
+                die.id === id ? { ...die, isHeld: !die.isHeld } : die,
+            ),
+        );
     }
 
-    const diceElements = dice.map(dieObj => (
+    const diceElements = dice.map((dieObj) => (
         <Die
             key={dieObj.id}
             value={dieObj.value}
             isHeld={dieObj.isHeld}
             hold={() => hold(dieObj.id)}
         />
-    ))
+    ));
 
     return (
         <main>
-            {gameWon && <Confetti
-            width={width}
-            height={height} />}
+            {gameWon && <Confetti width={width} height={height} />}
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container">
-                {diceElements}
-            </div>
+            <p className="instructions">
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+            </p>
+            <div className="dice-container">{diceElements}</div>
             <button className="roll-dice" onClick={rollDice}>
                 {gameWon ? "New Game" : "Roll"}
             </button>
         </main>
-    )
+    );
 }
